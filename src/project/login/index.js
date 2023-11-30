f8009cc302e64c343b717f0dd82811a99a6b30d7
David Mitre <david@Davids-MacBook-Pro.local>
2023-11-29 22:43:01 -0500

    addition of login, sign up, sign out and current profile features - bare bones

--- src/project/login/index.js
+++ src/project/login/index.js
@@ -1,9 +1,54 @@
 import { Link } from "react-router-dom";
+import * as client from "./client";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function Login() {
+  const [error, setError] = useState("");
+  const [credentials, setCredentials] = useState({ username: "", password: "" });
+  const navigate = useNavigate();
+  const signin = async () => {
+    console.log(credentials);
+    await client.signin(credentials);
+    navigate("/profile"); //could go to profile - id ?
+  };
+  const signup = async () => {
+    try {
+      console.log(credentials);
+      await client.signup(credentials);
+      navigate("/profile"); // could navigate to profile-id as well
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
+
+  const signupAsBusiness = async () => {
+    try {
+      console.log(credentials);
+      credentials.role = 'JOB-POSTER';
+      await client.signup(credentials);
+      navigate("/profile"); // could navigate to profile-id as well
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
+
+
   return (
     <div>
       <h1>Login screen</h1>
+      {error && <div>{error}</div>}
+      <input placeholder="Username" value={credentials.username} onChange={(e) => setCredentials({...credentials, username: e.target.value})}/>
+      <br/>
+      <input placeholder="Password" type="password" value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})}/>
+      <br/>
+      <button onClick={signin}> Sign in </button>
+      <button onClick={signup}> Sign up </button>
+      <h3>To Post Positions...</h3>
+      <button onClick={signupAsBusiness}> Sign up as Business</button>
+      <br/>
+      <br/>
       <Link to="/home">Home screen</Link><br/>
       <Link to="http://localhost:4000">Server</Link>
     </div>
--- src/project/profile/current.js
+++ src/project/profile/current.js
@@ -1,11 +1,54 @@
 import { Link } from "react-router-dom";
+import * as userclient from "../login/client";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 
 function Current() {
-  const user = "user1"
+  const user = "user1" //todo remove
+  const { id } = useParams();
+  const [account, setAccount] = useState(null);
+  const navigate = useNavigate();
+
+
+
+  //functions
+  const fetchAccount = async () => {
+    const account = await userclient.account();
+    setAccount(account);
+  };
+
+  const findUserById = async (id) => {
+    const user = await userclient.findUserById(id);
+    setAccount(user);
+  };
+
+  const signout = async () => {
+    await userclient.signout();
+    navigate("/"); //todo go home?? whatever we want ig
+  };
+
+
+  //use effect
+
+  useEffect(() => {
+    if (id) {
+        findUserById(id);
+      } else {
+        fetchAccount();
+      }
+    }, []);
+
+
+
   return (
     <div>
       <h1>Current profile screen</h1>
+      {account && (<h2>{account.username}'s profile screen!!</h2>)}
+      {account && (<h3>{account.role} Account</h3>)}
       <Link to={user}>{user}'s profile screen</Link>
+      <br/>
+      {account && (<button onClick={signout}> Sign Out </button>)}
     </div>
   )
 }

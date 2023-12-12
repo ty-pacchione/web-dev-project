beb813bec848117670d8275b5513efb804337bfd
jamesonho <ho.jam@northeastern.edu>
2023-12-11 22:12:52 -0500

    Improved Search Visuals

--- src/project/search/JSearchSearch.js
+++ src/project/search/JSearchSearch.js
@@ -1,10 +1,15 @@
 import { JobSearch } from "../JSearchService";
 import { useState, useEffect } from "react";
-import { Link, useNavigate, useSearchParams, useLocation } from "react-router-dom";
+import {
+  Link,
+  useNavigate,
+  useSearchParams,
+  useLocation,
+} from "react-router-dom";
 
 function JSearchSearch() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [query, setQuery] = useState(searchParams.get('query') || '');
+  const [query, setQuery] = useState(searchParams.get("query") || "");
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -14,7 +19,7 @@ function JSearchSearch() {
 
   const search = async (text) => {
     setSearchParams({
-      query: text
+      query: text,
     });
     const response = await JobSearch(text);
     console.log(response);
@@ -22,71 +27,89 @@ function JSearchSearch() {
   };
 
   useEffect(() => {
-    if (query !== '') {
+    if (query !== "") {
       //need to pick out the right data
       //setSearchTerm(searchParams);
       if (!results) {
-          // const tempquery = searchParams.get
-          // search();
-          
-          search(query);
-          console.log('no results but yes search params')
-      } 
-      
+        // const tempquery = searchParams.get
+        // search();
+
+        search(query);
+        console.log("no results but yes search params");
+      }
     }
   }, [searchParams]);
- 
-//(e) => setSearchTerm(e.target.value)
 
-const handleChange = (e) => {
-  setQuery(e.target.value);
-  console.log(query);
-};
+  //(e) => setSearchTerm(e.target.value)
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    console.log(query);
+  };
 
   return (
     <div>
       <h1>Job Search</h1>
-      <input
-        type="text"
-        value={query}
-        className="form-control"
-        onChange={handleChange}
-      />
-      <button className="btn btn-primary" onClick={() => search(query)}>
-        Search
-      </button>
-      <div className="list-group">
-        {results && 
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: 20,
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          className="form-control"
+          placeholder="Search by keywords"
+          onChange={handleChange}
+          style={{ width: "50%", marginRight: 10 }}
+        />
+        <button className="btn btn-primary" onClick={() => search(query)}>
+          Search
+        </button>
+      </div>
+      <div style={{ marginBottom: 10 }} className="list-group">
+        {results &&
           results.data &&
           results.data.map((job) => (
-            <div key={job.job_id} className="list-group-item list-group-item-secondary">
-              <Link style={{ textDecoration: 'none', color: 'black' }} to={`/details/${job.job_id}`}>
-                <h2>{job.employer_name}</h2>
-                <div>
-                  {job.employer_logo && <img className="float-start" style={{width: '150px',
-  height: 'auto'}} src={job.employer_logo} />}
-                  <h3 className="float-start">{job.job_title}</h3>
-                  
-                  <div className="float-end">{job.job_id}</div>
-
-                  <h3 className="float-end">{job.job_city}</h3>
-                  <h3 className="float-end">{job.job_state}</h3>
-                  <h3 className="float-end">{job.job_country}</h3>
+            <Link
+              key={job.job_id}
+              to={`/details/${job.job_id}`}
+              className="list-group-item"
+              style={{
+                backgroundColor: "white",
+                border: "1px solid black",
+                marginLeft: "10%",
+                marginRight: "10%",
+              }}
+            >
+              <h5>{job.job_title}</h5>
+              <div className="d-flex">
+                {job.employer_logo && (
+                  <img
+                    style={{ width: "100px" }}
+                    src={job.employer_logo}
+                    alt="Employer Logo"
+                  />
+                )}
+                <div style={{ paddingLeft: 20 }}>
+                  <div>
+                    <h6>{job.employer_name}</h6>
+                  </div>
+                  <div className="d-flex">
+                    {job.job_city}
+                    {job.job_city && job.job_state && <span>,&nbsp;</span>}
+                    {job.job_state}
+                    {(job.job_city || job.job_state) && job.job_country && (
+                      <span>,&nbsp;</span>
+                    )}
+                    {job.job_country}
+                  </div>
                 </div>
-              </Link>
-
-            </div>
-          ))
-          // results.search.data.albums.map((album) => (
-          //   <div key={album.id}>
-          //     <Link to={`/project/album-details/${album.id}`}>
-          //       <img src={albumImageUrl(album)} />
-          //       <h2>{album.name}</h2>
-          //     </Link>
-          //   </div>))
-          //results is array 
-          
-          }
+              </div>
+            </Link>
+          ))}
       </div>
     </div>
   );
